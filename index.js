231d28e50c1aaeee5baeac3c44c2272519b4b0c9
Jashandeep Singh <jashan.cheema98@gmail.com>
2020-01-11 17:11:16 -0400

    drag and drop
    1)Play again button working now.
    2)Score woking now

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ var dealerScore = 0;
 var aww = new Audio("aww.mp3");
 var swish = new Audio("swish.m4a");
 var cash = new Audio("cash.mp3");
-
+var hitBtnBlock = false;
+var standBtnBlock = false;
+var noOfRandoms = 0;
+var winCount = 0;
+var lossCount = 0;
+var drawCount = 0;
 function yourHit() {
-  var random = generateRandomNo();
+  if (hitBtnBlock == false) {
+    var random = generateRandomNo();
 
-  var img = document.createElement("img");
-  var div = document.getElementById("images");
-  img.src = imagesdata(random);
-  img.height = 150;
-  img.width = 100;
-  div.appendChild(img);
-  swish.play();
-  yourScore = yourScore + random;
+    var img = document.createElement("img");
+    var div = document.getElementById("images");
+    img.src = imagesdata(random);
+    img.height = 150;
+    img.width = 100;
+    div.appendChild(img);
+    swish.play();
+    yourScore = yourScore + random;
 
-  var e = (document.getElementById("urScoreId").innerHTML = yourScore);
-  if (yourScore > 21) {
-    document.getElementById("status").innerHTML = "Busted";
-    aww.play();
+    var e = (document.getElementById("urScoreId").innerHTML = yourScore);
+    if (yourScore > 21) {
+      document.getElementById("status").innerHTML = "Busted";
+      lossCount++;
+      document.getElementById("loss").innerHTML = lossCount;
+      aww.play();
+    }
   }
 }
 
@@ -40,53 +49,93 @@ function imagesdata(random) {
 }
 
 function dealerHit() {
-  // dealerScore = dealerScore + generateRandomNo();
-  var noOfRandoms = Math.floor(Math.random() * 3) + 2;
+  hitBtnBlock = true;
+  if (standBtnBlock == false) {
+    if (yourScore <= 21) {
+      // dealerScore = dealerScore + generateRandomNo();
+      noOfRandoms = Math.floor(Math.random() * 3) + 2;
 
-  console.log("total cards" + noOfRandoms);
-  for (var i = 0; i < noOfRandoms; i++) {
-    var randomNo = generateRandomNo();
-    console.log("Number generated" + randomNo);
-    var img = document.createElement("img");
-    var div = document.getElementById("dealerImages");
-    img.src = imagesdata(randomNo);
-    dealerScore = dealerScore + randomNo;
-    img.height = 150;
-    img.width = 100;
-    div.appendChild(img);
-    swish.play();
-  }
+      console.log("total cards" + noOfRandoms);
+      for (var i = 0; i < noOfRandoms; i++) {
+        var randomNo = generateRandomNo();
+        console.log("Number generated" + randomNo);
+        var img = document.createElement("img");
+        var div = document.getElementById("dealerImages");
+        img.src = imagesdata(randomNo);
+        dealerScore = dealerScore + randomNo;
+        img.height = 150;
+        img.width = 100;
+        div.appendChild(img);
+        swish.play();
+      }
+      standBtnBlock = true;
+      var e = (document.getElementById(
+        "dealerScoreId"
+      ).innerHTML = dealerScore);
 
-  var e = (document.getElementById("dealerScoreId").innerHTML = dealerScore);
+      if (yourScore > dealerScore) {
+        if (yourScore > 21) {
+          document.getElementById("status").innerHTML = "You Loose";
+          aww.play();
+        } else if (yourScore <= 21) {
+          document.getElementById("status").innerHTML = "You Win";
+          winCount++;
+          document.getElementById("win").innerHTML = winCount;
+          cash.play();
+        }
+      } else if (yourScore == dealerScore) {
+        document.getElementById("status").innerHTML = "Match Tie";
+        drawCount++;
+        document.getElementById("draw").innerHTML = drawCount;
+      } else if (dealerScore > yourScore) {
+        if (dealerScore > 21) {
+          document.getElementById("status").innerHTML = "You Win";
 
-  if (yourScore > dealerScore) {
-    if (yourScore > 21) {
-      document.getElementById("status").innerHTML = "You Loose";
-      aww.play();
-    } else if (yourScore <= 21) {
-      document.getElementById("status").innerHTML = "You Win";
-      cash.play();
-    }
-  } else if (yourScore == dealerScore) {
-    document.getElementById("status").innerHTML = "Match Tie";
-  } else if (dealerScore > yourScore) {
-    if (dealerScore > 21) {
-      document.getElementById("status").innerHTML = "You Win ";
-      cash.play();
-    } else if (dealerScore <= 21) {
-      document.getElementById("status").innerHTML = "You Loose";
-      aww.play();
+          winCount++;
+          document.getElementById("win").innerHTML = winCount;
+
+          cash.play();
+        } else if (dealerScore <= 21) {
+          document.getElementById("status").innerHTML = "You Loose";
+          lossCount++;
+          document.getElementById("loss").innerHTML = lossCount;
+          aww.play();
+        }
+      }
+    } else {
+      document.getElementById("status").innerHTML =
+        "You Loose,because you went above 21";
+      lossCount++;
+      document.getElementById("loss").innerHTML = lossCount;
     }
   }
 }
 
 function clearAll() {
+  hitBtnBlock = false;
+  standBtnBlock = false;
   console.log("clearall");
-  document.getElementById("images").remove();
-  document.getElementById("dealerImages").remove();
+
+  var yourImages = document
+    .querySelector("#human-cards")
+    .querySelectorAll("img");
+  var dealerImg = document
+    .querySelector("#dealerImages")
+    .querySelectorAll("img");
+
+  for (var i = 0; i < yourImages.length; i++) {
+    yourImages[i].remove();
+    console.log("removed image no" + i);
+  }
+
+  for (var i = 0; i < dealerImg.length; i++) {
+    dealerImg[i].remove();
+    console.log("removed image no" + i);
+  }
   document.getElementById("urScoreId").innerHTML = "";
   document.getElementById("dealerScoreId").innerHTML = "";
   document.getElementById("status").innerHTML = "Lets Play";
+
   yourScore = 0;
   dealerScore = 0;
 }
